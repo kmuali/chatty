bb52de6c4451897756ad146a5578a4ca78e71325
kmualic <kmualic>
2022-09-08 15:12:34 +0200

    added on submit refresh

--- assets/js/chat.ajax.js
+++ assets/js/chat.ajax.js
@@ -38,8 +38,10 @@ function sendForm() {
     isOn = true;
     toggleEmojis();
     if (msgInput.value) {
-        scrollChatBody();
         msgInput.value = "";
+        setTimeout(() => {
+            refreshMessages();
+        }, 300);
     }
     return false;
 }
@@ -54,6 +56,7 @@ function refreshMessages() {
         }
     }
     http.send(`q=getMessages`);
+    scrollChatBody();
     return false;
 }
 
